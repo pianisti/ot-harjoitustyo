539166e602707458975356bb9127dd119ce3ad1e
pianisti <192371238+pianisti@users.noreply.github.com>
2026-03-24 13:37:50 +0200

    week 2 task 6

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -1,3 +1,10 @@
+#Kortin saldo alussa oikein !!!
+#Rahan lataaminen kasvattaa saldoa oikein !!!
+#Rahan ottaminen toimii:
+#Saldo vähenee oikein, jos rahaa on tarpeeksi !!!
+#Saldo ei muutu, jos rahaa ei ole tarpeeksi !!!
+#Metodi palauttaa True, jos rahat riittivät ja muuten False
+
 import unittest
 from maksukortti import Maksukortti
 
@@ -7,3 +14,24 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(1000)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 20.0)
+
+    def test_saldo_vahenee_jos_kortilla_rahaa(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 5.0)
+
+    def test_saldo_sama_jos_kortilla_ei_rahaa(self):
+        self.maksukortti.ota_rahaa(10000)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.0)
+
+    def test_metodi_true_jos_kortilla_rahaa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(100), True)
+
+    def test_metodi_false_jos_kortilla_ei_rahaa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(10000), False)
